9171c73a86e4228a82fbc239c2730eab6078cd53
Mitchell Falkow <mdfalkow@gmail.com>
2020-03-28 14:38:54 -0400

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import DashboardLayout from './components/common/DashboardLayout'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  props: {
+    MuiButton: {
+      size: 'small',
+      variant: 'outlined',
+      color: 'primary'
+    }
+  }
+})
 
 export default function App() {
   return (
     <>
+      <MuiThemeProvider theme={theme}>
+        <DashboardLayout>
+          
+        </DashboardLayout>
+      </MuiThemeProvider>
     </>
   )
 }

ad69695f4e920ffe02cfded25711e8512fb502fa
Owen Xie <owenxie24@gmail.com>
2020-03-28 23:53:47 -0400

    Add automatic centering

--- frontend/src/components/common/MapContainer.js
+++ frontend/src/components/common/MapContainer.js
@@ -8,44 +8,63 @@ import {
 import Hospital from "./Hospital";
 import Axios from "axios";
 
+const style = {
+  width: "100%",
+  height: "100%"
+};
+
 export class MapContainer extends React.Component {
   constructor(props) {
     super(props);
+    const bounds = new this.props.google.maps.LatLngBounds();
     this.state = {
       hospitals: [],
       showingInfoWindow: false,
       activeMarker: {},
-      selectedPlace: {}
+      selectedPlace: {},
+      bounds
     };
   }
 
   componentDidMount() {
     // setTimeout(
-    //   () =>
+    //   () => {
+    //     const hospitals = this.state.hospitals
+    //         .concat(new Hospital("location1", "test", 42.02, -77.01))
+    //         .concat(new Hospital("location2", "Example Hospital", 41.00, -77.01))
+    //
+    //     const newBounds = new this.props.google.maps.LatLngBounds();
+    //     hospitals.forEach(hospital => newBounds.extend({lat: hospital.position.lat, lng: hospital.position.lng}));
+    //
     //     this.setState({
-    //       hospitals: this.state.hospitals
-    //           .concat(new Hospital("location1", "test", 42.02, -77.01))
-    //           .concat(new Hospital("location2", "Example Hospital", 41.00, -77.01))
-    //     }),
-    //   3000
+    //       hospitals,
+    //       bounds: newBounds
+    //     });
+    //   }, 3000
     // );
 
     Axios.get("/hospitals")
         .then(response => {
+          const hospitals = response.data.map(hospital =>
+              new Hospital(
+                  hospital._id,
+                  hospital.name,
+                  hospital.location.latitude,
+                  hospital.location.longitude
+              )
+          );
+
+          const newBounds = new this.props.google.maps.LatLngBounds();
+          hospitals.forEach(hospital => newBounds.extend({lat: hospital.position.lat, lng: hospital.position.lng}));
+
           this.setState({
-            hospitals: response.data.map(hospital =>
-                new Hospital(
-                    hospital._id,
-                    hospital.name,
-                    hospital.location.latitude,
-                    hospital.location.longitude
-                )
-            )
-          })
+            hospitals,
+            bounds: newBounds
+          });
         }, error => {
-          window.alert("Unable to load hospital locations. Please try again.")
+          window.alert("Unable to load hospital locations. Please try again.");
           console.log(error);
-        })
+        });
   }
 
   onMarkerHover(props, marker, e) {
@@ -79,10 +98,12 @@ export class MapContainer extends React.Component {
     return (
       <Map
           google={this.props.google}
+          style={style}
           initialCenter={{
             lat: 42.02,
             lng: -77.01
           }}
+          bounds={this.state.bounds}
       >
         {this.state.hospitals.map(hospital => (
           <Marker

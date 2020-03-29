b5f59f7da0493e116545f19dffb5b1ead8853794
Covenant <cfaluyi@gmail.com>
2020-03-28 21:32:04 -0400

    Stopped onMarkerHver  from changin the state multiple times. Implmented mouseOut function

--- frontend/src/components/common/MapContainer.js
+++ frontend/src/components/common/MapContainer.js
@@ -1,61 +1,85 @@
-import React from 'react';
-import {GoogleApiWrapper, InfoWindow, Map, Marker} from "google-maps-react";
+import React from "react";
+import {
+  GoogleApiWrapper,
+  InfoWindow,
+  Map,
+  Marker,
+  Coordinates
+} from "google-maps-react";
 import Hospital from "./Hospital";
 
 export class MapContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hospitals: [],
-            showingInfoWindow: false,
-            activeMarker: {},
-            selectedPlace: {},
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      hospitals: [],
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {}
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(
+      () =>
+        this.setState({
+          hospitals: this.state.hospitals.concat(
+            new Hospital("test", 42.02, -77.01)
+          )
+        }),
+      3000
+    );
+  }
+
+  onMarkerHover(props, marker, e) {
+    console.log(props);
+    if (!this.state.showingInfoWindow) {
+      if (props !== this.state.selectedPlace) {
+        this.setState({
+          selectedPlace: props,
+          activeMarker: marker,
+          showingInfoWindow: true
+        });
+      }
     }
+  }
 
-    componentDidMount() {
-        setTimeout(() => this.setState({
-            hospitals: this.state.hospitals.concat(new Hospital("test", 42.02, -77.01))
-        }), 3000);
+  onMarkerHoverOut(props, marker, e) {
+    console.log(props);
+    if (props !== this.state.selectedPlace) {
+      this.setState({
+        selectedPlace: {},
+        activeMarker: {},
+        showingInfoWindow: false
+      });
     }
+  }
 
-    onMarkerHover(props, marker, e) {
-        console.log(props);
-        if(props !== this.state.selectedPlace) {
-            this.setState({
-                selectedPlace: props,
-                activeMarker: marker,
-                showingInfoWindow: true
-            });
-        }
-    }
+  render() {
+    return (
+      <Map google={this.props.google}>
+        {this.state.hospitals.map(hospital => (
+          <Marker
+            key={hospital.name}
+            position={hospital.position}
+            onMouseover={this.onMarkerHover.bind(this)}
+            onMouseout={this.onMarkerHoverOut.bind(this)}
+          />
+        ))}
 
-
-    render() {
-        return (
-            <Map google={this.props.google}>
-                { this.state.hospitals.map(hospital =>
-                        <Marker
-                            key={hospital.name}
-                            name={hospital.name}
-                            position={hospital.position}
-                            onMouseover={this.onMarkerHover.bind(this)}
-                        />
-                )}
-
-                <InfoWindow
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}>
-                    <div>
-                        <h1>{this.state.selectedPlace.name}</h1>
-                    </div>
-                </InfoWindow>
-            </Map>
-        );
-    }
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
 }
 
 export default GoogleApiWrapper({
-    apiKey: "",
+  apiKey: "AIzaSyBjSmWzmgK2NIHkBtYQ7GBcozEkt3T5Pn4"
 })(MapContainer);
-
